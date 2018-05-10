b12f0eb620116f6ce31aaad8ca5806a443ae5535
Sai Bulusu <sai.bulusu@gmail.com>
2018-05-09 18:39:41 -0700

    Finished basic console version of the game

--- RubiksCubeSolver/src/console/Rubiks.java
+++ RubiksCubeSolver/src/console/Rubiks.java
@@ -8,6 +8,8 @@ public class Rubiks {
 		init();
 		displayCube();
 		scramble();
+//		System.out.println("Start solving");
+//		solve();
 	}
 
 	// hold the data of each side of the cube
@@ -26,10 +28,19 @@ public class Rubiks {
 		moves.add("D");
 		moves.add("R");
 		moves.add("L");
+		moves.add("F");
+		moves.add("B");
+		moves.add("U'");
+		moves.add("D'");
+		moves.add("R'");
+		moves.add("L'");
+		moves.add("F'");
+		moves.add("B'");
 
 		// randomly select an algorithm to run 20 times
 		for (int i = 0; i < 20; i++) {
-			int rand = (int) (Math.random() * 4);
+			int rand = (int) (Math.random() * 12);
+			System.out.println(moves.get(rand));
 			turn(moves.get(rand));
 		}
 	}
@@ -89,15 +100,12 @@ public class Rubiks {
 		System.out.println("      " + top[1][0] + top[1][1] + top[1][2]);
 		System.out.println("      " + top[2][0] + top[2][1] + top[2][2]);
 		System.out.println("L    F    R");
-		System.out.println("" + left[0][0] + left[0][1] + left[0][2] + "  " + 
-		front[0][0] + front[0][1] + front[0][2]
+		System.out.println("" + left[0][0] + left[0][1] + left[0][2] + "  " + front[0][0] + front[0][1] + front[0][2]
 				+ "  " + right[0][0] + right[0][1] + right[0][2]);
-		System.out.println("" + left[1][0] + left[1][1] + left[1][2] + "  " + 
-				front[1][0] + front[1][1] + front[1][2]
-				+ "  " + right[0][0] + right[0][1] + right[0][2]);
-		System.out.println("" + left[2][0] + left[2][1] + left[2][2] + "  " + 
-				front[2][0] + front[2][1] + front[2][2]
-				+ "  " + right[0][0] + right[0][1] + right[0][2]);
+		System.out.println("" + left[1][0] + left[1][1] + left[1][2] + "  " + front[1][0] + front[1][1] + front[1][2]
+				+ "  " + right[1][0] + right[1][1] + right[1][2]);
+		System.out.println("" + left[2][0] + left[2][1] + left[2][2] + "  " + front[2][0] + front[2][1] + front[2][2]
+				+ "  " + right[2][0] + right[2][1] + right[2][2]);
 		System.out.println("     D");
 		System.out.println("      " + down[0][0] + down[0][1] + down[0][2]);
 		System.out.println("      " + down[1][0] + down[1][1] + down[1][2]);
@@ -118,27 +126,32 @@ public class Rubiks {
 
 	// turn the cube according to the given algorithm
 	static void turn(String algorithm, int ignore) {
-		// FIX: rotate the given side for each algorithm
 		if (algorithm.equals("U")) {
-			char[] f = front[0];
-			char[] l = left[0];
-			char[] b = back[0];
-			char[] r = right[0];
+			char[] f = copy(front[0]);
+			char[] l = copy(left[0]);
+			char[] b = copy(back[0]);
+			char[] r = copy(right[0]);
 
 			front[0] = r;
 			left[0] = f;
 			back[0] = l;
 			right[0] = b;
+			rotate(top);
+			rotate(top);
+			rotate(top);
 		} else if (algorithm.equals("D")) {
-			char[] f = front[2];
-			char[] l = left[2];
-			char[] b = back[2];
-			char[] r = right[2];
+			char[] f = copy(front[2]);
+			char[] l = copy(left[2]);
+			char[] b = copy(back[2]);
+			char[] r = copy(right[2]);
 
 			front[2] = l;
 			left[2] = b;
 			back[2] = r;
 			right[2] = f;
+			rotate(down);
+			rotate(down);
+			rotate(down);
 		} else if (algorithm.equals("L")) {
 			char[] f = { front[0][0], front[1][0], front[2][0] };
 			char[] d = { down[0][0], down[1][0], down[2][0] };
@@ -157,6 +170,9 @@ public class Rubiks {
 			top[0][0] = b[0];
 			top[1][0] = b[1];
 			top[2][0] = b[2];
+			rotate(left);
+			rotate(left);
+			rotate(left);
 		} else if (algorithm.equals("R")) {
 			char[] f = { front[0][2], front[1][2], front[2][2] };
 			char[] d = { down[0][2], down[1][2], down[2][2] };
@@ -175,29 +191,141 @@ public class Rubiks {
 			top[0][2] = f[0];
 			top[1][2] = f[1];
 			top[2][2] = f[2];
+			rotate(right);
+			rotate(right);
+			rotate(right);
 		} else if (algorithm.equals("F")) {
-
+			char[] u = { top[2][0], top[2][1], top[2][2] };
+			char[] r = { right[0][0], right[1][0], right[2][0] };
+			char[] d = { down[0][0], down[0][1], down[0][2] };
+			char[] l = { left[0][2], left[1][2], left[2][2] };
+			
+			right[0][0] = u[0];
+			right[1][0] = u[1];
+			right[2][0] = u[2];
+			down[0][0] = r[0];
+			down[0][1] = r[1];
+			down[0][2] = r[2];
+			left[0][2] = d[0];
+			left[1][2] = d[1];
+			left[2][2] = d[2];
+			top[2][0] = l[0];
+			top[2][1] = l[1];
+			top[2][2] = l[2];
+			rotate(front);
+			rotate(front);
+			rotate(front);
 		} else if (algorithm.equals("B")) {
-
+			char[] u = { top[0][0], top[0][1], top[0][2] };
+			char[] r = { right[0][2], right[1][2], right[2][2] };
+			char[] d = { down[2][0], down[2][1], down[2][2] };
+			char[] l = { left[0][0], left[1][0], left[2][0] };
+			
+			right[0][0] = d[0];
+			right[1][0] = d[1];
+			right[2][0] = d[2];
+			down[0][0] = l[0];
+			down[0][1] = l[1];
+			down[0][2] = l[2];
+			left[0][2] = u[0];
+			left[1][2] = u[1];
+			left[2][2] = u[2];
+			top[2][0] = r[0];
+			top[2][1] = r[1];
+			top[2][2] = r[2];
+			rotate(back);
+			rotate(back);
+			rotate(back);
 		} else {
 			if (algorithm.equals("U'")) {
 				turn("U", -1);
+				turn("U", -1);
+				turn("U", -1);
 			} else if (algorithm.equals("D'")) {
+				turn("D", -1);
+				turn("D", -1);
 				turn("D", -1);
 			} else if (algorithm.equals("L'")) {
 				turn("L", -1);
+				turn("L", -1);
+				turn("L", -1);
 			} else if (algorithm.equals("R'")) {
+				turn("R", -1);
+				turn("R", -1);
 				turn("R", -1);
 			} else if (algorithm.equals("F'")) {
 				turn("F", -1);
+				turn("F", -1);
+				turn("F", -1);
 			} else if (algorithm.equals("B'")) {
+				turn("B", -1);
+				turn("B", -1);
 				turn("B", -1);
 			}
 		}
 	}
 
+	// helper method to rotate a 2d array clockwise
+	static void rotate(char[][] array) {
+		for (int x = 0; x < array.length / 2; x++) {
+			for (int y = x; y < array.length - x - 1; y++) {
+				char temp = array[x][y];
+				array[x][y] = array[y][array.length - 1 - x];
+				array[y][array.length - 1 - x] = array[array.length - 1 - x][array.length - 1 - y];
+				array[array.length - 1 - x][array.length - 1 - y] = array[array.length - 1 - y][x];
+				array[array.length - 1 - y][x] = temp;
+			}
+		}
+	}
+
+	// helper method to check if one side is solved
+	static boolean oneSideSolved(char[][] side) {
+		char color = side[0][0];
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				if (side[r][c] != color) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	// helper method to return a copy of a char array
+	static char[] copy(char[] array) {
+		char[] copy = new char[array.length];
+		for (int i = 0; i < copy.length; i++) {
+			copy[i] = array[i];
+		}
+		return copy;
+	}
+	
+	// check if the cube is solved
+	static boolean solved() {
+		return oneSideSolved(top) && oneSideSolved(back) && oneSideSolved(front) && oneSideSolved(left)
+				&& oneSideSolved(right) && oneSideSolved(down);
+	}
+	
 	// solve the cube
 	static void solve() {
+		// brute force solve, but should use an algorithm
+		while (!solved()) {
+			ArrayList<String> moves = new ArrayList<String>();
+			moves.add("U");
+			moves.add("D");
+			moves.add("R");
+			moves.add("L");
+			moves.add("F");
+			moves.add("B");
+			moves.add("U'");
+			moves.add("D'");
+			moves.add("R'");
+			moves.add("L'");
+			moves.add("F'");
+			moves.add("B'");
 
+			int rand = (int) (Math.random() * 12);
+			turn(moves.get(rand));
+		}
 	}
 }
