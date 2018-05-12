cc5aefeef88afe56a26b7c27384b5d79c91f660d
Sai Bulusu <sai.bulusu@gmail.com>
2018-05-11 22:47:43 -0700

    Fridrich Method Step 1

--- RubiksCubeSolver/src/console/RubiksCube.java
+++ RubiksCubeSolver/src/console/RubiksCube.java
@@ -15,23 +15,19 @@ public class RubiksCube {
 	
 	public RubiksCube() {
 		init();
-		connected = new ArrayList<RubiksCube>();
-		
 		moves.add("U");
 		moves.add("D");
 		moves.add("R");
 		moves.add("L");
 		moves.add("F");
 		moves.add("B");
-		moves.add("U'");
-		moves.add("D'");
-		moves.add("R'");
-		moves.add("L'");
-		moves.add("F'");
-		moves.add("B'");
+		moves.add("u");
+		moves.add("d");
+		moves.add("r");
+		moves.add("l");
+		moves.add("f");
+		moves.add("b");
 	}
-	
-	ArrayList<RubiksCube> connected;
 	
 	// hold the data of each side of the cube
 	char[][] left;
@@ -41,25 +37,11 @@ public class RubiksCube {
 	char[][] front;
 	char[][] back;
 	
+	// each of the twelve moves that can happen for any given cube
 	ArrayList<String> moves = new ArrayList<String>();
 
 	// scramble the cube
 	void scramble() {
-		// moves contains the strings representing the algorithms
-		ArrayList<String> moves = new ArrayList<String>();
-		moves.add("U");
-		moves.add("D");
-		moves.add("R");
-		moves.add("L");
-		moves.add("F");
-		moves.add("B");
-		moves.add("U'");
-		moves.add("D'");
-		moves.add("R'");
-		moves.add("L'");
-		moves.add("F'");
-		moves.add("B'");
-
 		// randomly select an algorithm to run 20 times
 		for (int i = 0; i < 20; i++) {
 			int rand = (int) (Math.random() * 12);
@@ -116,38 +98,13 @@ public class RubiksCube {
 		}
 	}
 	
-	public void initConnected() {
-		connected = new ArrayList<RubiksCube>();
-		
-		moves.add("U");
-		moves.add("D");
-		moves.add("R");
-		moves.add("L");
-		moves.add("F");
-		moves.add("B");
-		moves.add("U'");
-		moves.add("D'");
-		moves.add("R'");
-		moves.add("L'");
-		moves.add("F'");
-		moves.add("B'");
-		
-		for (int i = 0; i < moves.size(); i++) {
-			turn(moves.get(i), -1);
-			connected.add(copyCube());
-			turn(moves.get(i), -1);
-			turn(moves.get(i), -1);
-			turn(moves.get(i), -1);
-		}
-	}
-
 	// testing method to display the cube
 	public String toString() {
 		String res = "";
 		res += "     T\n";
-		res += "      " + top[0][0] + top[0][1] + top[0][2] + "\n";
-		res += "      " + top[1][0] + top[1][1] + top[1][2] + "\n";
-		res += "      " + top[2][0] + top[2][1] + top[2][2] + "\n";
+		res += "     " + top[0][0] + top[0][1] + top[0][2] + "\n";
+		res += "     " + top[1][0] + top[1][1] + top[1][2] + "\n";
+		res += "     " + top[2][0] + top[2][1] + top[2][2] + "\n";
 		res += "L    F    R\n";
 		res += "" + left[0][0] + left[0][1] + left[0][2] + "  " + front[0][0] + front[0][1] + front[0][2]
 				+ "  " + right[0][0] + right[0][1] + right[0][2] + "\n";
@@ -156,19 +113,27 @@ public class RubiksCube {
 		res += "" + left[2][0] + left[2][1] + left[2][2] + "  " + front[2][0] + front[2][1] + front[2][2]
 				+ "  " + right[2][0] + right[2][1] + right[2][2] + "\n";
 		res += "     D\n";
-		res += "      " + down[0][0] + down[0][1] + down[0][2] + "\n";
-		res += "      " + down[1][0] + down[1][1] + down[1][2] + "\n";
-		res += "      " + down[2][0] + down[2][1] + down[2][2] + "\n";
+		res += "     " + down[0][0] + down[0][1] + down[0][2] + "\n";
+		res += "     " + down[1][0] + down[1][1] + down[1][2] + "\n";
+		res += "     " + down[2][0] + down[2][1] + down[2][2] + "\n";
 		res += "     B\n";
-		res += "      " + back[0][0] + back[0][1] + back[0][2] + "\n";
-		res += "      " + back[1][0] + back[1][1] + back[1][2] + "\n";
-		res += "      " + back[2][0] + back[2][1] + back[2][2] + "\n";
+		res += "     " + back[0][0] + back[0][1] + back[0][2] + "\n";
+		res += "     " + back[1][0] + back[1][1] + back[1][2] + "\n";
+		res += "     " + back[2][0] + back[2][1] + back[2][2] + "\n";
 		res += "\n";
 		return res;
 	}
 
+	// turn the cube according to each character within the string given
+	private void turns(String turn) {
+		for (int i = 0; i < turn.length(); i++) {
+			turn(turn.charAt(i) + "");
+		}
+	}
+	
 	// turn the cube according to a certain given algorithm
-	private void turn(String algorithm) {
+	void turn(String algorithm) {
+		System.out.println("algorithm is " + algorithm);
 		// by calling with an ignore value, displaying the cube is easier
 		turn(algorithm, -1);
 		System.out.println(this);
@@ -248,7 +213,7 @@ public class RubiksCube {
 			char[] u = { top[2][0], top[2][1], top[2][2] };
 			char[] r = { right[0][0], right[1][0], right[2][0] };
 			char[] d = { down[0][0], down[0][1], down[0][2] };
-			char[] l = { left[0][2], left[1][2], left[2][2] };
+			char[] l = { left[2][2], left[1][2], left[0][2] };
 			
 			right[0][0] = u[0];
 			right[1][0] = u[1];
@@ -266,48 +231,48 @@ public class RubiksCube {
 			rotate(front);
 			rotate(front);
 		} else if (algorithm.equals("B")) {
-			char[] u = { top[0][0], top[0][1], top[0][2] };
+			char[] u = { top[0][2], top[0][1], top[0][0] };
 			char[] r = { right[0][2], right[1][2], right[2][2] };
-			char[] d = { down[2][0], down[2][1], down[2][2] };
+			char[] d = { down[2][2], down[2][1], down[2][0] };
 			char[] l = { left[0][0], left[1][0], left[2][0] };
 			
-			right[0][0] = d[0];
-			right[1][0] = d[1];
-			right[2][0] = d[2];
-			down[0][0] = l[0];
-			down[0][1] = l[1];
-			down[0][2] = l[2];
-			left[0][2] = u[0];
-			left[1][2] = u[1];
-			left[2][2] = u[2];
-			top[2][0] = r[0];
-			top[2][1] = r[1];
-			top[2][2] = r[2];
+			right[0][2] = d[0];
+			right[1][2] = d[1];
+			right[2][2] = d[2];
+			down[2][0] = l[0];
+			down[2][1] = l[1];
+			down[2][2] = l[2];
+			left[0][0] = u[0];
+			left[1][0] = u[1];
+			left[2][0] = u[2];
+			top[0][0] = r[0];
+			top[0][1] = r[1];
+			top[0][2] = r[2];
 			rotate(back);
 			rotate(back);
 			rotate(back);
 		} else {
-			if (algorithm.equals("U'")) {
+			if (algorithm.equals("u")) {
 				turn("U", -1);
 				turn("U", -1);
 				turn("U", -1);
-			} else if (algorithm.equals("D'")) {
+			} else if (algorithm.equals("d")) {
 				turn("D", -1);
 				turn("D", -1);
 				turn("D", -1);
-			} else if (algorithm.equals("L'")) {
+			} else if (algorithm.equals("l")) {
 				turn("L", -1);
 				turn("L", -1);
 				turn("L", -1);
-			} else if (algorithm.equals("R'")) {
+			} else if (algorithm.equals("r")) {
 				turn("R", -1);
 				turn("R", -1);
 				turn("R", -1);
-			} else if (algorithm.equals("F'")) {
+			} else if (algorithm.equals("f")) {
 				turn("F", -1);
 				turn("F", -1);
 				turn("F", -1);
-			} else if (algorithm.equals("B'")) {
+			} else if (algorithm.equals("b")) {
 				turn("B", -1);
 				turn("B", -1);
 				turn("B", -1);
@@ -378,8 +343,85 @@ public class RubiksCube {
 				&& oneSideSolved(right) && oneSideSolved(down);
 	}
 	
-	
-	void solve() {
+	// first step: gather the white edges
+	private void whiteEdges() {
+		boolean whiteEdgesInPlace = front[0][1] == 'W' && front[1][0] == 'W' && front[1][2] == 'W'
+				&& front[2][1] == 'W' && left[1][2] == 'B' && right[1][0] == 'G' && down[0][1] == 'O'
+				&& top[2][1] == 'R';
+		
+		while (!whiteEdgesInPlace) {
+			if (back[1][0] == 'W') {
+				turns("B");
+			} else if (back[1][2] == 'W') {
+				turns("b");
+			} else if (back[2][1] == 'W') {
+				turns("BB");
+			} else if (left[0][1] == 'W') {
+				turns("UbuB");
+			} else if (left[1][0] == 'W') {
+				turns("LUbuBl");
+			} else if (left[1][2] == 'W') {
+				turns("LLb");
+			} else if (left[2][1] == 'W') {
+				turns("Lbl");
+			} else if (right[0][1] == 'W') {
+				turns("uBUb");
+			} else if (right[1][0] == 'W') {
+				turns("RuBUb");
+			} else if (right[1][2] == 'W') {
+				turns("ruBUbR");
+			} else if (right[2][1] == 'W') {
+				turns("RRuBUbRR");
+			} else if (front[0][1] == 'W' && top[2][1] != 'R') {
+				turns("UU");
+			} else if (front[1][0] == 'W' && left[1][2] != 'B') {
+				turns("FUUf");
+			} else if (front[1][2] == 'W' && right[1][0] != 'G') {
+				turns("fUUF");
+			} else if (front[2][1] == 'W' && down[0][1] != 'O') {
+				turns("FFUUFF");
+			} else if (top[0][1] == 'W') {
+				turns("BldLBB");
+			} else if (top[1][0] == 'W') {
+				turns("UBuldLBB");
+			} else if (top[1][2] == 'W') {
+				turns("uBUldLBB");
+			} else if (top[2][1] == 'W') {
+				turns("uuBldLBB");
+			} else if (down[0][1] == 'W') {
+				turns("DrBR");
+			} else if (down[1][0] == 'W') {
+				turns("DDrBRd");
+			} else if (down[1][2] == 'W') {
+				turns("dDrBRD");
+			} else if (down[2][1] == 'W') {
+				turns("ddDrBRDD");
+			}
 
+			System.out.println("checkpoint");
+			if (back[0][1] == 'W') {
+				char adjacent = top[0][1];
+				if (adjacent == 'B') {
+					turns("BLL");
+				} else if (adjacent == 'R') {
+					turns("UU");
+				} else if (adjacent == 'G') {
+					turns("bRR");
+				} else {
+					turns("BBDD");
+				}
+			}
+			System.out.println(this);
+			System.out.println("next step");
+			
+			whiteEdgesInPlace = front[0][1] == 'W' && front[1][0] == 'W' && front[1][2] == 'W'
+					&& front[2][1] == 'W' && left[1][2] == 'B' && right[1][0] == 'G' && down[0][1] == 'O'
+					&& top[2][1] == 'R';
+		}
+	}
+	
+	// solve the cube using the Fridrich method
+	void solve() {
+		whiteEdges();
 	}
 }
--- RubiksCubeSolver/src/console/Tester.java
+++ RubiksCubeSolver/src/console/Tester.java
@@ -1,9 +1,19 @@
 package console;
 
+import java.util.Scanner;
+
 public class Tester {
 
 	public static void main(String[] args) {
 		RubiksCube cube = new RubiksCube();
+		System.out.println(cube);
+		cube.scramble();
+		Scanner scan = new Scanner(System.in);
+		if (scan.next().equals("solve")) {
+			System.out.println("START SOLVING NOW\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+			cube.solve();
+		}
+		cube.solve();
 		System.out.println(cube);
 	}
 }
