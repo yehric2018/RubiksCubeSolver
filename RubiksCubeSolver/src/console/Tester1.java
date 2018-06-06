2004b8e93060dc60b78fdd6ce0d7b11da6c46826
Sai Bulusu <sai.bulusu@gmail.com>
2018-06-05 17:59:25 -0700

    Added a jar file

--- RubiksCubeSolver/src/console/Tester1.java
+++ RubiksCubeSolver/src/console/Tester1.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Tester1 {
 
-	public static void main(String[] args) {
-		RubiksCube cube = new RubiksCube();
-		
-		for (int i = 0; i < 100; i++) {
-			cube.scramble();
-			cube.solve();
-		}
-	}
+//	public static void main(String[] args) {
+//		RubiksCube cube = new RubiksCube();
+//		
+//		for (int i = 0; i < 100; i++) {
+//			cube.scramble();
+//			cube.solve();
+//		}
+//	}
 	
 }
